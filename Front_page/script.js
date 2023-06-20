deadc0c5706870e0924bdd170a9dc2f1b65df501
Hemanshu Parmar <hemanshup2002@gmail.com>
2023-06-20 12:37:29 +0530

    Hemanshu added all files in temp branch

---
+++ Front_page/script.js
@@ -0,0 +1,33 @@
+function redirect(){
+setTimeout(function() {
+    window.location.href ="loginPage.html";
+},5000);
+}
+
+function skip(){
+    window.location.href = "homepage.html"
+}
+
+function register(){
+    window.location.href = "registerpage.html"
+}
+
+function restaurant(){
+    window.location.href = "venueList.html"
+}
+
+function book(){
+    window.location.href = "slotBookingPage.html"
+}
+
+function payment(){
+    window.location.href = "payment.html"
+}
+
+function token_confirmation(){
+    window.location.href = "token_confirm.html"
+}
+
+function signup(){
+    window.location.href = "token_confirm.html"
+}
